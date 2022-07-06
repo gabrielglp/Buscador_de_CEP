ebbb2aad16b5ab16dd0d8edce8e4a3c167106ed4
Gabriel de Souza Martins <83505259+gabrielglp@users.noreply.github.com>
2022-07-05 23:09:47 -0300

    Buscador de CEP
    Fiz as marcações em HMTL
    e estilizei em CSS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { FiSearch } from 'react-icons/fi'
+import './styles.css'
+
 function App() {
   return (
-    <div className="App">
-      <h1>teste</h1>
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input type="text" placeholder="Digite seu cep..." />
+
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#FFF" />
+        </button>
+      </div>
+
+      <main className="main">
+        <h2>CEP: 11714200</h2>
+
+        <span>Rua Teste da Silva</span>
+        <span>Complemento: Algum complemento</span>
+        <span>Santa Rosa</span>
+        <span>Copacabana - RJ</span>
+      </main>
     </div>
   )
 }
