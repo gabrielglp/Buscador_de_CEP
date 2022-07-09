e87abd2bcfd2011c9ed6e499f6fcdfc7a2fa6e91
Gabriel de Souza Martins <83505259+gabrielglp@users.noreply.github.com>
2022-07-08 23:50:36 -0300

    Concluído
    Para fechar coloquei a interação usando React JS, importando uma API da Viacep.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,58 @@
+import { useState } from 'react'
 import { FiSearch } from 'react-icons/fi'
 import './styles.css'
 
+import api from './services/api'
+
 function App() {
+  const [input, setInput] = useState('')
+  const [cep, setCep] = useState({})
+
+  async function handleSearch() {
+    if (input === '') {
+      alert('Preencha algum cep!')
+      return
+    }
+
+    try {
+      const response = await api.get(`${input}/json`)
+      setCep(response.data)
+      setInput('')
+    } catch {
+      alert('Ops erro ao buscar.')
+      setInput('')
+    }
+  }
+
   return (
     <div className="container">
       <h1 className="title">Buscador CEP</h1>
 
       <div className="containerInput">
-        <input type="text" placeholder="Digite seu cep..." />
+        <input
+          type="text"
+          placeholder="Digite seu cep..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
 
-        <button className="buttonSearch">
+        <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size={25} color="#FFF" />
         </button>
       </div>
 
-      <main className="main">
-        <h2>CEP: 11714200</h2>
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+          <h2>CEP: {cep.cep}</h2>
 
-        <span>Rua Teste da Silva</span>
-        <span>Complemento: Algum complemento</span>
-        <span>Santa Rosa</span>
-        <span>Copacabana - RJ</span>
-      </main>
+          <span>{cep.logradouro}</span>
+          <span>Complemento: {cep.complemento}</span>
+          <span>{cep.bairro}</span>
+          <span>
+            {cep.localidade} - {cep.uf}
+          </span>
+        </main>
+      )}
     </div>
   )
 }
